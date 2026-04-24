9e9c8e002c89e68c70205712352f1af06b3d70b9
245985948 <245985948@users.noreply.github.com>
2026-04-24 20:43:48 +0800

    chore: remove deprecated client-template files

--- client-template/network/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 网络层导出
- * 在 Cocos Creator 中使用: import { network, gameClient, NetworkState } from './network';
- */
-
-export { NetworkManager, NetworkState, network } from './NetworkManager';
-export { GameClient, gameClient } from './GameClient';
-export { GameConfig } from './GameConfig';
-export * from './MessageCodes';
